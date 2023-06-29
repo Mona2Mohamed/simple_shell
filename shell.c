e4f17e1bb5854c8ba65917608720b598957de980
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-06-28 20:55:19 -0700

    updating

--- shell.c
+++ shell.c
@@ -1,61 +1,42 @@
-#include "shell.h"
-/**
- * main - entry point
- * @argc: the number of command-line arguments
- * @argv: array of strings containing the command-line arguments
- * return: 0 success
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+/*
+ * main - main
+ * retrun : 0
  */
-int main(int argc, char **argv)
-{
-	char *promptLine = "simpleshell:";
-	char *inputLine = NULL;
-	size_t inputSize = 0;
-	int n_tkn = 0;
-	char *argPtr_cpy = NULL;
-	char *str_tkn = NULL;
-	ssize_t getlineReturn = 0;
-	int i;
-	const char *delim = " \n";
+int main() {
+    char* command = NULL;
+    size_t command_size = 0;
+    pid_t pid;
 
-	(void) argc;
-	while (1)
-	{
-		printf("%s", promptLine);
-		getlineReturn =  getline(&inputLine, &inputSize, stdin);
-		if (getlineReturn == -1)
-		{
-			printf("out of shell\n");
-			return (-1);
-		}
-
-		argPtr_cpy = malloc(sizeof(char) * getlineReturn);
-		if (argPtr_cpy == NULL)
-		{
-			perror("memory allocation error");
-			return (-1);
-		}
-		strcpy(argPtr_cpy, inputLine);
-		str_tkn = strtok(argPtr_cpy, delim);
-		while (str_tkn != NULL)
-		{
-			n_tkn++;
-			str_tkn = strtok(NULL, delim);
-		}
-		n_tkn++;
-		argv = malloc(sizeof(char *) * n_tkn);
-		str_tkn = strtok(inputLine, delim);
-
-		for (i = 0; str_tkn != NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char) * strlen(str_tkn));
-			strcpy(argv[i], str_tkn);
-			str_tkn = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-		execute_cmd(argv);
-	}
-
-	free(argPtr_cpy);
-	free(inputLine);
-	return (0);
+    while (1) {
+        printf("simple_shell>$ ");
+        if (getline(&command, &command_size, stdin) == -1) {
+            break;
+        }
+        command[strcspn(command, "\n")] = '\0';
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
+        pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+	    } else if (pid == 0) {
+            execlp(command, command, NULL);
+            printf("%s: command not found\n", command);
+            exit(EXIT_FAILURE);
+        } else {
+            /*Parent process*/
+            waitpid(pid, NULL, 0);
+        }
+    }
+    free(command);
+    printf("\n");
+    return 0;
 }
